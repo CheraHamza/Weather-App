f524b73e0a5e5847f78e51ddca20db60701b003a
CheraHamza <cherahamza1@gmail.com>
2024-06-27 17:01:45 +0100

    add functions to fetch and process weather forcast

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
 import "../css/main.css";
+
+async function getForcastInfoForCity(city) {
+	city = city.toLowerCase();
+	try {
+		const response = await fetch(
+			`https://api.weatherapi.com/v1/forecast.json?key=52b1149f972d4a12843142300242606 &q=${city}&days=3`,
+			{ mode: "cors" }
+		);
+
+		if (response.ok) {
+			return await response.json();
+		}
+	} catch (error) {
+		return error;
+	}
+}
+
+async function processForcastInfo() {
+	const forcastInfoJson = await getForcastInfoForCity(
+		prompt("choose city?", "jijel")
+	);
+
+	console.log(forcastInfoJson);
+}
+
+// demo ui
+
+const fetchDataButton = document.querySelector("button");
+
+fetchDataButton.addEventListener("click", () => {
+	processForcastInfo();
+});
